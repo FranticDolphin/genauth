dc013152886ed433b4fdcd9671dd865dcc1c68de
Dennis Woo <dwoo@sent.com>
2017-09-20 17:31:38 -0700

    test stubs

---
+++ test.js
@@ -0,0 +1,74 @@
+var genauth = require('./');
+var mocha = require('mocha');
+var chai = require('chai');
+var should = chai.should();
+
+describe('genauth', function(){
+  describe('methods', function(){
+    it('should accept valid input from CLI', (done => {
+    })
+
+    it('should throw an error if CLI input is not valid', (done => {
+    })
+
+  })
+
+  describe('user model functions', function (){
+    it('should create a new user', (done => {
+    })
+
+    it('should not accept an invalid user', (done => {
+    })
+
+    it('should make sure emails are unique', (done => {
+    })
+
+    it('should find user by email', (done => {
+    })
+
+    it('should allow a user to delete their account', (done => {
+    })
+
+  })
+
+  describe('sessions model functions', function (){
+    it('should create a new session', (done => {
+    })
+
+    it('should end a session', (done => {
+    })
+
+  })
+
+  describe('sessions web UI creationk', function (){
+    it('should render a reg page', (done => {
+    })
+
+    it('should render a login page', (done => {
+    })
+
+    it('should redirect to home after user logs out', (done => {
+    })
+
+    it('should redirect to intended page if login is successful', (done => {
+    })
+
+    it('should redirect to intended page if login is successful', (done => {
+    })
+
+    it('should check for email confirmation', (done => {
+    })
+
+    it('should render a forgot my password page', (done => {
+    })
+
+
+
+
+    it('should end a ', (done => {
+    })
+
+  })
+
+
+})
